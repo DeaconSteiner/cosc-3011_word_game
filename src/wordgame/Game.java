a663208ee32eea36b4057a3e643346e69ca2fdcf
DeaconSteiner <deaconsteiner.it@gmail.com>
2026-01-31 14:52:18 -0700

    Continue work on class implementations

--- src/wordgame/Game.java
+++ src/wordgame/Game.java
@@ -1,15 +1,71 @@
 package wordgame;
 
+import java.util.List;
+import java.util.Scanner;
+import wordgame.Feedback;
+
 public class Game {
 
     private Word secret;
     private WordList wordList;
 
-    public void start() {}
+    public void start() {
+        List<String> words = List.of(
+            "Apple",
+            "Brave",
+            "Crown",
+            "Flame",
+            "Globe",
+            "Hasty",
+            "Jolly",
+            "Knock",
+            "Lunar",
+            "Mirth",
+            "Noble",
+            "Oasis",
+            "Plumb",
+            "Quest",
+            "Royal",
+            "Slate",
+            "Trace",
+            "Unity",
+            "Vigor",
+            "Whale",
+            "Xerus",
+            "Youth",
+            "Zebra",
+            "Blunt",
+            "Crisp",
+            "Drift",
+            "Elbow",
+            "Fancy",
+            "Grape",
+            "Hazel"
+        );
 
-    public void makeGuess(String guess) {}
+        wordList = new WordList(words);
+        secret = wordList.getRandom();
+    }
 
-    public void printFeedback(Feedback fb) {}
+    public void makeGuess(String guess) {
+        Word guessWord = new Word(guess);
+        Feedback fb = guessWord.compareTo(secret);
+        printFeedback(fb);
 
-    public static void main(String[] args) {}
+        if (fb.isCorrect()) {
+            System.out.println("Congratulations! You guessed the word!");
+        }
+    }
+
+    public void printFeedback(Feedback fb) {
+        System.out.println(fb);
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+
+        Scanner scanner = new Scanner(System.in);
+        boolean guessedCorrect = false;
+    }
 }
