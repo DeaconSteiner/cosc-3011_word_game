4dd88f43be893684dc72d6fcbf880a5d48c5bdf6
DeaconSteiner <deaconsteiner.it@gmail.com>
2026-02-02 17:43:27 -0700

    Start work on compareTo logic in Word class

--- src/wordgame/Word.java
+++ src/wordgame/Word.java
@@ -14,7 +14,25 @@ class Word {
         }
     }
 
-    public Feedback compareTo(Word other) {}
+    public Feedback compareTo(Word other) {
+        int lCorrect = 0; // # of correct letters
+        int pCorrect = 0; // # of letters in correct position
+
+        // Used to check for wrong position, right letter
+        ArrayList<Character> unmatched = new ArrayList<>();
+
+        for (int i = 0; i < other.length(); i++) {
+            if (this.letters.get(i).equals(other.letters.get(i))) {
+                pCorrect++;
+            } else {
+                unmatched.add(other.letters.get(i));
+            }
+        }
+
+        //TODO: Figure out how to check for correct letter, wrong position
+
+        return new Feedback(lCorrect, pCorrect);
+    }
 
     public int length() {
         return letters.size();
